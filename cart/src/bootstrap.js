8bb997081f164ffc1bdb5e2818a5b3689f72237f
necatiozmen <necatiozmn@gmail.com>
2022-05-11 20:36:52 +0300

    Add isolation option

--- cart/src/bootstrap.js
+++ cart/src/bootstrap.js
@@ -1,5 +1,15 @@
 import faker from "faker";
 
-const cartText = `<div>${faker.random.number()}</div>`
+const mount = (el) => {
+    const cartText = `<div>${faker.random.number()}</div>`
+    el.innerHTML = cartText
+}
 
-document.querySelector('#dev-cart').innerHTML = cartText
+if (process.env.NODE_ENV === "development") {
+    const el = document.querySelector('#dev-cart');
+    if (el) {
+        mount(el);
+    }
+}
+
+export { mount }
